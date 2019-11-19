49e7d6688e79e61096435cc5c306f4fdb5a79f12
David <awesomesauce712@gmail.com>
2019-11-19 11:19:54 -0800

    Add project files.

---
+++ DndCharacterSheet/Data/CharacterSheetModel.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DndCharacterSheet.Data
+{
+    public class CharacterSheetModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        public int level { get; set; }
+        public int baseStrength { get; set; }
+        public int currentStrength { get; set; }
+        public int strengthMod { get; set; } = 0;
+        public int baseDexterity { get; set; }
+        public int currentDexterity { get; set; }
+        public int dexterityMod { get; set; } = 0;
+        public int baseConstitution { get; set; }
+        public int currentConstitution { get; set; }
+        public int constitutionMod { get; set; } = 0;
+        public int baseIntelligence { get; set; }
+        public int currentIntelligence { get; set; }
+        public int intelligenceMod { get; set; } = 0;
+        public int baseWisdom { get; set; }
+        public int currentWisdom { get; set; }
+        public int wisdomMod { get; set; } = 0;
+        public int baseCharisma { get; set; }
+        public int currentCharisma { get; set; }
+        public int charismaMod { get; set; } = 0;
+        public int inspiration { get; set; } = 0;
+        public int profBonus { get; set; } = 2;
+        public int maxHitpoints { get; set; }
+        public int acutalHitPoints { get; set; }
+        public int hitDie { get; set; }
+        public int speed { get; set; }
+        public int tempHitPoints { get; set; } = 0;
+        public int NumbSuccDeathSave { get; set; } = 0;
+        public int NumbFailDeathSave { get; set; } = 0;
+        public int baseArmorClass { get; set; } = 10;
+        public int currentArmorClass {get; set;} = 0;
+        public int armorClassModifier { get; set; } = 0;
+
+        public CharacterSheetModel(int level,int healthDie,int walkingSpeed = 30)
+        {
+            this.speed = walkingSpeed;
+
+            this.hitDie = healthDie;
+            Random random = new Random();
+            List<int> rolls = new List<int>();
+            int amountUnder10 = 0;
+            for (int i = 0; i < 7; i = i + 1)
+            {
+                var potRolls = new List<int>();
+                potRolls.Add(random.Next(0, 6));
+                potRolls.Add(random.Next(0, 6));
+                potRolls.Add(random.Next(0, 6));
+                potRolls.Add(random.Next(0, 6));
+                potRolls.OrderByDescending(x => x).Take(rolls.Count - 1).ToList();
+
+                var roll = potRolls.Sum();
+                if (roll < 10 && amountUnder10 >= random.Next(0,3))
+                {
+                    while(roll <= 10)
+                    {
+                        potRolls = new List<int>();
+                        potRolls.Add(random.Next(0, 6));
+                        potRolls.Add(random.Next(0, 6));
+                        potRolls.Add(random.Next(0, 6));
+                        potRolls.Add(random.Next(0, 6));
+                        potRolls.OrderByDescending(x => x).Take(rolls.Count - 1).ToList();
+
+                        roll = potRolls.Sum();
+                    }
+                }
+                rolls.Add(roll);
+            }
+            List<int> statsToPick = new List<int> { 0, 1, 2, 3, 4 ,5};
+            rolls.OrderByDescending(x => x).Take(rolls.Count - 2).ToList();
+            for (int i = 0; i < 6; i = i + 1)
+            {
+           
+                var statToFill = statsToPick[random.Next(0, statsToPick.Count)];
+                switch (statToFill)
+                {
+                    case (0):
+                        this.baseCharisma = rolls.First();
+                        this.currentWisdom = this.baseCharisma;
+                        statsToPick.Remove(0);
+                        break;
+                    case (1):
+                        this.baseWisdom = rolls.First();
+                        this.currentWisdom = this.baseWisdom;
+                        statsToPick.Remove(1);
+                        break;
+                    case (2):
+                        this.baseIntelligence = rolls.First();
+                        this.currentIntelligence = this.baseIntelligence;
+                        statsToPick.Remove(2);
+                        break;
+                    case (3):
+                        this.baseConstitution = rolls.First();
+                        this.currentConstitution = this.baseConstitution;
+                        statsToPick.Remove(3);
+                        break;
+                    case (4):
+                        this.baseDexterity = rolls.First();
+                        this.currentDexterity = this.baseDexterity;
+                        statsToPick.Remove(4);
+                        break;
+                    case (5):
+                        this.baseStrength = rolls.First();
+                        this.currentStrength = this.baseStrength;
+                        statsToPick.Remove(5);
+                        break;
+                }
+                rolls.RemoveAt(0);
+            }
+
+            this.baseArmorClass = 10 + ((this.currentDexterity % 10) / 2);
+            this.currentArmorClass = this.baseArmorClass;
+
+            var hitPoints = 0;
+            for(int i = 0;i<level;i++)
+            {
+                hitPoints += this.baseConstitution + (random.Next(1, this.hitDie));
+            }
+            this.maxHitpoints = (baseConstitution + hitDie)+ hitPoints;
+        }
+
+        private void changeArmorClass(int changeAcBy)
+        {
+            if(armorClassModifier + changeAcBy <= 0)
+            {
+                this.armorClassModifier = 0;
+                this.currentArmorClass = this.baseArmorClass;
+            }
+            else
+            {
+                this.armorClassModifier += changeAcBy;
+                this.currentArmorClass = this.armorClassModifier + this.baseArmorClass;
+            }
+        }
+    }
+}
