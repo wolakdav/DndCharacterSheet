53925f55243618fa4c5fa58aeca792b2cd97ee64
wolakdav <awesomesauce712@gmail.com>
2019-11-21 17:08:47 -0800

    Some app state stuff

---
+++ DndCharacterSheet/Data/AppState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DndCharacterSheet.Data
+{
+    public class AppState
+    {
+        public CharacterSheetModel character { get; private set; }
+        public event Action OnChange;
+        public void SetCharacter(CharacterSheetModel newChar)
+        {
+            character = newChar;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+    }
+}
--- DndCharacterSheet/Startup.cs
+++ DndCharacterSheet/Startup.cs
@@ -29,6 +29,7 @@ namespace DndCharacterSheet
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddTransient<AppState>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
